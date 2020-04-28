2d4a3029e3b3f75d2efa1ece6494a63a85b9d752
isaacs <i@izs.me>
2020-04-28 12:29:30 -0700

    update tweets with new archive format and new tweets

--- parse-tweets.js
+++ parse-tweets.js
@@ -1,18 +1,11 @@
-const parse = require('csv-parse')
-const parser = parse({
-  columns: true,
-})
-
-parser.on('data', obj => {
-  if (obj.retweeted_status_id || obj.text.match(/^RT @/) || obj.in_reply_to_user_id)
-    return
-  console.log(obj.text
+const tweets = require('./tweet.js')
+for (const {tweet} of tweets) {
+  if (tweet.retweeted_status_id || tweet.full_text.match(/^RT @/) || tweet.in_reply_to_user_id)
+    continue
+  console.log(tweet.full_text
     .replace(/@[^ ]+ /, '')
     .replace(/\n/g, ' ')
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
   )
-})
-
-const { createReadStream } = require('fs')
-createReadStream(__dirname + '/tweets.csv').pipe(parser)
+}
